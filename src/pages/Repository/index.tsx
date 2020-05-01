e06ca890f2bacbc78b62909f2422192f52d5c54d
Rômulo V. Pereira <romulocgd@hotmail.com>
2020-05-01 14:18:38 -0300

    Navigation between routes and styling details

--- src/pages/Repository/index.tsx
+++ src/pages/Repository/index.tsx
@@ -1,7 +1,66 @@
 import React from 'react';
+import { useRouteMatch, Link } from 'react-router-dom';
+import { FiChevronLeft, FiChevronRight } from 'react-icons/fi';
+
+import logoImg from '../../assets/logo.svg';
+import { Header, RepositoryInfo, Issues } from './styles';
+
+interface RepositoryParams {
+  repository: string;
+}
 
 const Repository: React.FC = () => {
-  return <h1>Repository</h1>;
-};
+  const { params } = useRouteMatch<RepositoryParams>();
 
+  return (
+    <>
+      <Header>
+        <img src={logoImg} alt="Github Explorer" />
+        <Link to="/dashboard">
+          <FiChevronLeft size={16} />
+          Voltar
+        </Link>
+      </Header>
+
+      <RepositoryInfo>
+        <header>
+          <img
+            src="https://avatars0.githubusercontent.com/u/13742415?v=4"
+            alt="{params.repository}"
+          />
+          <div>
+            <strong>serverless</strong>
+            <p>serverless/serverless</p>
+          </div>
+        </header>
+        <ul>
+          <li>
+            <strong>1800</strong>
+            <span>Stars</span>
+          </li>
+          <li>
+            <strong>1800</strong>
+            <span>Forks</span>
+          </li>
+          <li>
+            <strong>1800</strong>
+            <span>Issues abertas</span>
+          </li>
+        </ul>
+      </RepositoryInfo>
+
+      <Issues>
+        <Link to="gdfgdfgdfg">
+          <div>
+            <strong>fdsdfsdf</strong>
+            <p>dfsdfdsf</p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </Link>
+      </Issues>
+    </>
+  );
+};
+// <h1>Repository:{params.repository}</h1>
 export default Repository;
